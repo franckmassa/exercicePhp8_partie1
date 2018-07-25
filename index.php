08b139c83bfff0daa00aaed82c98e8a3b7c95a52
massa <massafranck@gmail.com>
2018-07-25 21:52:28 +0200

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$add = 3 + 4;
+$multi = 5 * 20;
+$divi =  45 / 5;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice8</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo $add.'</br> '.$multi.'</br>'.$divi;
+    ?>
+  </p>
+</body>
+</html>
